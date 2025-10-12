6f16485b4be087f93adff7d94a4292b0279019bd
David Pinto's <juan.david.pinto@correounivalle.edu.co>
2025-10-12 17:17:42 -0500

    added about us view

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from '../pages/LandingPage/LandingPage';
 import { MoviesPage } from '../pages/MoviesPage/MoviesPage';
+import { AboutPage } from '../pages/AboutPage';
 
 export const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/movies" element={<MoviesPage />} />
+      <Route path="/about" element={<AboutPage />} />
     </Routes>
   </BrowserRouter>
 );
