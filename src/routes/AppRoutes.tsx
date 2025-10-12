15094d3cd97f57a6fab563e587bfa87897f71d58
vilhood <otalvarokev@gmail.com>
2025-10-12 15:24:35 -0500

    added new views

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from '../pages/LandingPage/LandingPage';
 import { MoviesPage } from '../pages/MoviesPage/MoviesPage';
+import ProfilePage from "../pages/ProfilePage/ProfilePage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
 
 export const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/movies" element={<MoviesPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   </BrowserRouter>
 );
