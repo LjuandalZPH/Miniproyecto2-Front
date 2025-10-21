0402db46c9f899db5e85e6a441ba0be716811903
David Pinto's <juan.david.pinto@correounivalle.edu.co>
2025-10-21 15:42:23 -0500

    changed route to api_url

--- src/services/pexelsService.ts
+++ src/services/pexelsService.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/pexels/videos";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = `${API_BASE_URL}/api/pexels/videos`;
 
 export const getVideosFromPexels = async () => {
   try {
     const response = await axios.get(API_URL, {
       params: {
-        query: "movie trailer", // 🔥 busca clips tipo trailers
+        query: "movie trailer",
         per_page: 10,
         min_duration: 5,
         max_duration: 30,
@@ -17,6 +18,6 @@ export const getVideosFromPexels = async () => {
     return response.data.videos || [];
   } catch (error: any) {
     console.error("Error al obtener videos desde Pexels:", error.message);
-    return [];
-  }
+    return [];
+  }
 };
